9122fbc1a6a3def626a10d092bf714ec5640f161
6nhuseyin <6nhuseyin@gmail.com>
2023-10-18 21:44:29 +0100

    learning templates

--- practicalgo/template/basic/main.go
+++ practicalgo/template/basic/main.go
@@ -13,16 +13,27 @@ type Product struct {
 }
 
 func redTeaPotHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("/home/haltin/studies/practicalgo/views/product.html")
-
+	tmpl, err := template.ParseFiles(
+		"/home/haltin/studies/practicalgo/views/product.html",
+		"/home/haltin/studies/practicalgo/views/header.html",
+		"/home/haltin/studies/practicalgo/views/footer.html",
+	)
 	if err != nil {
 		http.Error(w, "Failed to parse the template", http.StatusInternalServerError)
 		log.Printf("Failed to parse template: %v", err)
 		return
 	}
+	pageTitle := "Red Tea Pot Page"
 	teaPot := Product{Name: "Red Tea Pot 250ml", Description: " llll Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun tut labore et dolore", Price: "19.99"}
 
-	err = tmpl.Execute(w, teaPot)
+	/*//err = tmpl.Execute(w, teaPot)
+	err = tmpl.ExecuteTemplate(w, "product.html", teaPot)*/
+	// Execute the "product.html" template and pass the data
+	err = tmpl.ExecuteTemplate(w, "product.html", struct {
+		Product
+		PageTitle string
+	}{teaPot, pageTitle})
+
 	// handle error
 	if err != nil {
 		http.Error(w, "Failed to execute the template", http.StatusInternalServerError)
